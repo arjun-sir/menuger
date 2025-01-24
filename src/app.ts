ab78dd6f776f2ae1e77941b988d794a370df0ac1
arjun-sir <arjunkhanna0108@gmail.com>
2025-01-24 12:58:48 +0530

    feat: added basic ip rate limiting

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import express from "express";
 import categoryRoutes from "./routes/category";
 import subCategoryRoutes from "./routes/subcategory";
 import itemRoutes from "./routes/item";
+import { apiRateLimiter } from "./middleware/rateLimiter";
 
 const app = express();
 app.use(express.json());
+
+app.use("/api", apiRateLimiter);
 
 app.use("/api/category", categoryRoutes);
 app.use("/api/subcategory", subCategoryRoutes);
